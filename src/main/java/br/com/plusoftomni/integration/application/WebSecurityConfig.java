44cd5c8af4c7d343f5eb251b3324f168b1c062c3
anunes <alannunes@plusoft.com.br>
2016-07-28 12:19:01 -0300

    ajustes https

--- src/main/java/br/com/plusoftomni/integration/application/WebSecurityConfig.java
+++ src/main/java/br/com/plusoftomni/integration/application/WebSecurityConfig.java
@@ -1,16 +1,10 @@
 package br.com.plusoftomni.integration.application;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig //extends WebSecurityConfigurerAdapter {
+{
+   /* @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -30,5 +24,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
-    }
+    }*/
 }
